66193203cd58bfb5648afddd2adf35e72db158b8
ujibayashi ryunosuke <ujibayashiryunosuke@ujibayashinoMacBook-Air.local>
2025-06-24 02:37:04 +0900

    set up 3d

--- src/index.php
+++ src/index.php
@@ -5,10 +5,25 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Title</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
    <!-- Content Here --> 
-    <h1>Hello world!!</h1>
+    <div class="banner">
+        <div class="slider" style="--quantity: 10">
+            <div class="item" style="--position: 1;"><img src="images/image-1.jpg" alt="image-1"></div>
+            <div class="item" style="--position: 2;"><img src="images/image-1.jpg" alt="image-2"></div>
+            <div class="item" style="--position: 3;"><img src="images/image-1.jpg" alt="image-3"></div>
+            <div class="item" style="--position: 4;"><img src="images/image-1.jpg" alt="image-4"></div>
+            <div class="item" style="--position: 5;"><img src="images/image-1.jpg" alt="image-5"></div>
+            <div class="item" style="--position: 6;"><img src="images/image-1.jpg" alt="image-6"></div>
+            <div class="item" style="--position: 7;"><img src="images/image-1.jpg" alt="image-7"></div>
+            <div class="item" style="--position: 8;"><img src="images/image-1.jpg" alt="image-8"></div>
+            <div class="item" style="--position: 9;"><img src="images/image-1.jpg" alt="image-9"></div>
+            <div class="item" style="--position: 10;"><img src="images/image-1.jpg" alt="image-10"></div>
+        </div>
+    </div>
     
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
